2d9e51ba7bc32c842069648e83c925789ed3e7a8
Shaziyehtajik <149049835+Shaziyehtajik@users.noreply.github.com>
2024-01-21 03:52:34 +0000

    Update stream.py

--- myflix-backend/stream.py
+++ myflix-backend/stream.py
@@ -6,15 +6,23 @@ class VideoStreamer:
 
     def initiate_stream(self, filename="Danger_River"):
         try:
-            if os.path.exists("output.log"):
-                os.remove("output.log")
+            self.cleanup_output_log()
 
             # Update the import path in the command
-            command = 'ffmpeg -re -i http://' + self.rtmp_server + '/mp4/' + filename + '.mp4 -vcodec copy -loop -1 -c:a aac -b:a 160k -ar 44100 -strict -2 -f flv rtmp://' + self.rtmp_server + '/stream/' + filename + ' > output.log 2>&1 < /dev/null &'
+            command = (
+                f'ffmpeg -re -i http://{self.rtmp_server}/mp4/{filename}.mp4 '
+                '-vcodec copy -loop -1 -c:a aac -b:a 160k -ar 44100 -strict -2 '
+                f'-f flv rtmp://{self.rtmp_server}/stream/{filename} > output.log 2>&1 < /dev/null &'
+            )
             os.system(command)
             return "success"
-        except:
-            return "error"
+        except Exception as e:
+            return f"error: {str(e)}"
+
+    @staticmethod
+    def cleanup_output_log():
+        if os.path.exists("output.log"):
+            os.remove("output.log")
 
 if __name__ == '__main__':
     # Update the rtmp_server parameter based on your actual server configuration
