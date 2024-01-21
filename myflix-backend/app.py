9bdb6f63245998117812569d96b9f39faf58a6d2
Shaziyehtajik <149049835+Shaziyehtajik@users.noreply.github.com>
2024-01-21 02:34:34 +0000

    Update app.py

--- myflix-backend/app.py
+++ myflix-backend/app.py
@@ -1,11 +1,12 @@
+import logging
 from flask import Flask
 from flask_cors import CORS
-from myflix-backend.api import api_page
+from myflix_backend.api import api_page
 
 app = Flask(__name__)
 
 # Load configuration from the specified file
-app.config.from_pyfile('myflix-backend/settings.py')
+app.config.from_pyfile('myflix_backend/settings.py')
 
 # Enable CORS for all routes
 CORS(app)
@@ -13,6 +14,12 @@ CORS(app)
 # Register the API blueprint
 app.register_blueprint(api_page)
 
+# Enable debugging mode
+app.debug = True
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)  # Set the log level to DEBUG
+
 if __name__ == "__main__":
-    # Run the application in debug mode on all available network interfaces
-    app.run(host='0.0.0.0', debug=True)
+    # Run the application on all available network interfaces
+    app.run(host='0.0.0.0')
