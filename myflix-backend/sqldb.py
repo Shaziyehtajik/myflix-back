fdc4d8d3719d1f4aea4728e5fd7018d99776f334
Shaziyehtajik <149049835+Shaziyehtajik@users.noreply.github.com>
2024-01-21 04:10:05 +0000

    Update sqldb.py

--- myflix-backend/sqldb.py
+++ myflix-backend/sqldb.py
@@ -1,18 +1,20 @@
-from flask import current_app, g, jsonify
+from flask import current_app, g
 from werkzeug.local import LocalProxy
 import pymysql
+from bson.json_util import dumps
 import bcrypt
 
 def get_sqldb():
     if 'sqldb' not in g:
         g.sqldb = pymysql.connect(
-            host=current_app.config.get('MYSQL_DATABASE_HOST'),
-            user=current_app.config.get('MYSQL_DATABASE_USER'),
-            password=current_app.config.get('MYSQL_DATABASE_PASSWORD'),
-            database=current_app.config.get('MYSQL_DATABASE_DB'),
+            host='sqldb.cr0gumy26ddi.us-west-2.rds.amazonaws.com',
+            user=current_app.config.get('MYSQL_USER'),
+            password=current_app.config.get('MYSQL_PASSWORD'),
+            database=current_app.config.get('MYSQL_DB'),
             port=3306
         )
     return g.sqldb
+
 
 # Use LocalProxy to read the global db instance with just `db`
 sqldb = LocalProxy(get_sqldb)
@@ -21,10 +23,14 @@ sqldb = LocalProxy(get_sqldb)
 
 def register_user(username, password):
     with sqldb.cursor() as cursor:
-        hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(10))
+        password = password.encode('utf-8')
+        hashed_pwd = bcrypt.hashpw(password, bcrypt.gensalt(10)) 
+        
         sql = "INSERT INTO users (username, password) VALUES (%s, %s)"
         values = (username, str(hashed_pwd, 'utf-8'))
         result = cursor.execute(sql, values)
+
+        # Committing the change to the DB
         sqldb.commit()
         return result == 1
 
@@ -40,33 +46,15 @@ def authenticate_user(username, password):
 
 def update_user_profile(username, new_password):
     with sqldb.cursor() as cursor:
-        hashed_pwd = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt(10))
+        new_password = new_password.encode('utf-8')
+        hashed_pwd = bcrypt.hashpw(new_password, bcrypt.gensalt(10))
+        
         sql = "UPDATE users SET password=%s WHERE username=%s"
         values = (str(hashed_pwd, 'utf-8'), username)
         result = cursor.execute(sql, values)
+
+        # Committing the change to the DB
         sqldb.commit()
         return result == 1
 
-# Additional functions for user movie interactions, admin functionalities, etc.
-
-def verify(username, password):
-    with sqldb.cursor() as cursor:
-        sql = "SELECT * FROM login WHERE username=%s"
-        cursor.execute(sql, (username,))
-        results = cursor.fetchone()
-
-        entered_password = password.encode('utf-8')
-
-        if results and bcrypt.checkpw(entered_password, results[2].encode('utf-8')):
-            return True
-        else:
-            return False
-
-def create(username, password):
-    with sqldb.cursor() as cursor:
-        hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(10))
-        sql = "INSERT INTO login (username, pass) VALUES (%s, %s)"
-        values = (username, str(hashed_pwd, 'utf-8'))
-        result = cursor.execute(sql, values)
-        sqldb.commit()
-        return result == 1
+# ... (Add more functions for user movie interactions, admin functionalities, etc.)
