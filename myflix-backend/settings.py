368d908c30d8d5098852bf64655c872b67de8ccd
Shaziyehtajik <149049835+Shaziyehtajik@users.noreply.github.com>
2024-01-21 03:42:50 +0000

    Update settings.py

--- myflix-backend/settings.py
+++ myflix-backend/settings.py
@@ -1,11 +1,11 @@
 import os
 
 # MongoDB Configuration
-MONGO_URI = "mongodb+srv://admin:adminpassword@cluster0.464wvua.mongodb.net/"
+MONGO_URI = os.getenv('MONGO_URI')
 
 # MySQL Configuration
-MYSQL_DATABASE_HOST = 'sqldb.cr0gumy26ddi.us-west-2.rds.amazonaws.com'
-MYSQL_DATABASE_DB = 'myflix_db'
-MYSQL_DATABASE_USER = os.getenv('admin')
-MYSQL_DATABASE_PASSWORD = os.getenv('adminpassword')
+MYSQL_DATABASE_HOST = os.getenv('MYSQL_HOST')
+MYSQL_DATABASE_DB = os.getenv('MYSQL_DB')
+MYSQL_DATABASE_USER = os.getenv('MYSQL_USER')
+MYSQL_DATABASE_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_DATABASE_PORT = 3306
