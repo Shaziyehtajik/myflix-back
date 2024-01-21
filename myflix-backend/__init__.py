42c2d9bafb125621d4acf04c26c144898465d2dc
Shaziyehtajik <149049835+Shaziyehtajik@users.noreply.github.com>
2024-01-21 04:26:27 +0000

    Update __init__.py

--- myflix-backend/__init__.py
+++ myflix-backend/__init__.py
@@ -1,6 +1,1 @@
-# __init__.py
-
-# This file can be empty if you don't need any package-level initialization.
-# It's required for Python to recognize the directory as a package.
-# You can also add package-level code or import statements if needed.
-
+touch myflix-back/myflix-backend/__init__.py
